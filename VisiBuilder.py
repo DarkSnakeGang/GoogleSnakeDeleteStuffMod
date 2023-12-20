4e25124c60e49badee25ac2b2801740970c1e797
Benny Yarmolovich <6286286+Yarmiplay@users.noreply.github.com>
2023-12-21 00:38:40 +0200

    Combined visibility into a single file

---
+++ VisiBuilder.py
@@ -0,0 +1,16 @@
+import urllib.request
+
+url = "https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakePudding/main/PuddingMod.js"  # Replace with the actual URL of the file you want to download
+destination_file = "PuddingMod.js"  # Replace with the desired local file name
+
+urllib.request.urlretrieve(url, destination_file)
+
+visi_file = open("VisibilityMod.js", "w", encoding='utf-8')
+visi_init = open("VisibilityInit.js", "r", encoding='utf-8')
+pudding = open("PuddingMod.js", "r", encoding='utf-8')
+
+visi_file.write(pudding.read())
+visi_file.write(visi_init.read())
+pudding.close()
+visi_init.close()
+visi_file.close()
